bfe7879abcc5a8ea7982429ef49ffbd924783781
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-12-12 13:38:13 -0600

    Fixed Image and added some styling

--- customer-support-app/src/Components/Header.js
+++ customer-support-app/src/Components/Header.js
@@ -1,23 +1,33 @@
 import Navbar from "react-bootstrap/Navbar";
+import Icon from "../assets/img/Icon_BLUE.png";
 
 function Header() {
   return (
     <>
       <Navbar
-        bg="light"
+        bg="dark"
         style={{
           borderBottom: "3px solid",
         }}
       >
         <Navbar.Brand>
           <img
-            src="../assets/img/Icon_Black.png"
+            src={Icon}
             width="50px"
             height="50px"
             className="d-inline-block align-top"
-            alt="React Bootstrap logo"
+            alt="supportLogo"
           />
         </Navbar.Brand>
+        <h3
+          className="text-white"
+          style={{
+            left: "0px",
+            right: "0px",
+          }}
+        >
+          Support Ticket Form
+        </h3>
       </Navbar>
     </>
   );
