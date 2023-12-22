33b37e1dffa11cdd5b1bf80e1470c0c2727745c2
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-12-22 12:32:54 -0600

    Added credentials for the database server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "",
-  port: 3000,
-  user: "",
-  password: "",
-  database: "",
+  host: "sandbox-1.cowdxaotesut.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "admin",
+  password: "password", //  need to create a new password & encrypt this with environmnement variable
+  database: "-",
 });
+
+db.connect((err) => {});
