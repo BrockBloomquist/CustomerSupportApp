8b11c1fffd4774981adc072a93b471b727960fa5
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2023-12-24 10:17:57 -0600

    Need to fix form validity because empty forms can be submitted still

--- customer-support-app/src/Components/Survey.js
+++ customer-support-app/src/Components/Survey.js
@@ -4,9 +4,11 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import Alert from "react-bootstrap/Alert";
 import "../Css Properties/Survey.css";
 
 export default function Survey() {
+  const [formSubmitted, setFormSubmitted] = useState(false);
   const id = uuidv4();
   const [bugForm, setBugForm] = useState({
     id: id,
@@ -34,6 +36,8 @@ export default function Survey() {
     if (form.checkValidity() === false) {
       event.stopPropagation();
     }
+    if (validated === true) {
+    }
     const fullName = bugForm.name;
     const email = bugForm.email;
     const details = bugForm.description;
@@ -49,11 +53,20 @@ export default function Survey() {
         details: details,
         ticketType: type,
       }),
+    }).then((res) => {
+      if (res.statusCode === 201) {
+        setFormSubmitted(true);
+      }
     });
   };
 
   return (
     <div>
+      {formSubmitted && (
+        <Alert key="success" variant="success">
+          Danger
+        </Alert>
+      )}
       <div>
         <h1>Feedback & Support</h1>
         <h6>
@@ -134,7 +147,7 @@ export default function Survey() {
 
           <Form.Group controlId="formFileMultiple" className="mb-3">
             <Form.Label>Attach Files (if any)</Form.Label>
-            <Form.Control type="file" multiple />
+            <Form.Control type="file" multiple accept="image/png, image/jpg" />
           </Form.Group>
         </Row>
         <Row className="align-self-center">
