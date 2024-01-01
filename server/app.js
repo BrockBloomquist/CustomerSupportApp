c56f6aaf96090067a6d0d4dea88c4cc9efe603c5
BrockBloomquist <104608812+BrockBloomquist@users.noreply.github.com>
2024-01-01 12:38:50 -0600

    Added comments

--- server/app.js
+++ server/app.js
@@ -10,10 +10,17 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong");
 });
 
+/**
+ * Allows the backend to listen on the local host port 3005
+ */
 app.listen(3005, () => {
   console.log("server is running on port 3005");
 });
 
+/**
+ * Creates a GET HTTP request to get all the tickets that exist in the database or
+ * each row/record in the database.
+ */
 app.get("/tickets", async (req, res) => {
   getTickets()
     .then((tickets) => {
@@ -24,11 +31,11 @@ app.get("/tickets", async (req, res) => {
     });
 });
 
-app.get("/getTicket", async (req, res) => {
-  const ticket = await getTicket();
-  res.send(ticket);
-});
-
+/**
+ * Creates a Post HTTP request on a specific server which creates a new ticket
+ * in the database endpoint table in AWS. If theres an error a 500 is sent to the
+ * server/client that made the request.
+ */
 app.post("/createTicket", async (req, res) => {
   const { id, fullName, email, details, ticketType } = req.body;
   createTicket(id, fullName, email, details, ticketType)
